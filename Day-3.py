f43bb8d0654bc3222e927cf686918de571b2cbb8
kukiey <akishabhujel@gmail.com>
2026-02-26 23:38:41 +0545

    Add files via upload

---
+++ Day-3.py
@@ -0,0 +1,11 @@
+# 53. Maximum Subarray
+class Solution(object):
+    def maxSubArray(self, nums):
+        maxSub = nums[0]
+        curSum = 0
+        for i in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += i
+            maxSub = max(maxSub, curSum)
+        return maxSub
