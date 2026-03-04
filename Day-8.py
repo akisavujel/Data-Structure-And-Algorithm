4856c90a58b24b40ecd82aab80f70e7f6bea5319
kukiey <akishabhujel@gmail.com>
2026-03-04 16:55:56 +0545

    Add files via upload

---
+++ Day-8.py
@@ -0,0 +1,14 @@
+# 344. Reverse string
+
+class Solution(object):
+    def reverseString(self, s):
+        l, r = 0, len(s)-1
+        while l < r:
+            s[l], s[r] = s[r],s[l]
+            l,r = l+1,r-1        
+
+'''
+Lmao finally ek week leetcode question gere paxi
+balla aja euta question buje .
+String simple xa solution😁 '''
+    
